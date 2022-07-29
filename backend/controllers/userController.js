c9b6f19ea1d57df564225f859dad36fffca28dd5
Ryan Santiago <ryan@ryans-mbp-16.attlocal.net>
2022-07-29 02:50:22 -0400

    Add mongoose functionality to register user and login user. Add error messaging.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,70 @@
-// Register user function used in user routes
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 
-// Login user function used in user routes
-const loginUser = (req, res) => {
-    res.send('Login Route')
-}
+// @desc    register a new user 
+// @route   /api/users
+// @access   public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    // Validation 
+    if(!name || !email || !password) {
+       res.status(400)
+       throw new Error('Please fill out all fields.')
+    }
+
+    // Find if user already exists
+    const userExists = await User.findOne({email})
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists. Login.')
+    }
+
+    // Hash the password 
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create user 
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })     
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data.')
+    }
+
+})
+
+// @desc    login user 
+// @route   /api/users/login
+// @access  public
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    // Check user and passwords match
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid credentials. Try again or register a new account.')
+    }
+})
 
 module.exports = {
     registerUser,
