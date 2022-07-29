be486d350c0d927e98f1c0f2f3938161c094ad1e
Ryan Santiago <ryan@ryans-mbp-16.attlocal.net>
2022-07-29 03:24:34 -0400

    Add json web token to the userController so that we can protect routes in the future with token authentication.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require('express-async-handler')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const User = require('../models/userModel')
 
@@ -37,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })     
     } else {
         res.status(400)
@@ -59,12 +61,19 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
         throw new Error('Invalid credentials. Try again or register a new account.')
     }
 })
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d' 
+    })
+}
 
 module.exports = {
     registerUser,
