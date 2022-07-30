9c4aab22ec9e8b05985b5196cf029b85cd3eb858
Ryan Santiago <ryan@ryans-mbp-16.attlocal.net>
2022-07-29 21:27:56 -0400

    Add structure for Redux frontend. Change server port to avoid conflicting issues on previous port.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 8001
 
 // Connect to mongodb database
 connectDB()
